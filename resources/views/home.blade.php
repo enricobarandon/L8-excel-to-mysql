0a2a5b2dae6bc7e2801ef378233ad4f8711515ed
samSAM <samuelbandola@gmail.com>
2022-03-26 15:17:16 +0800

    admin lte template design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,23 +3,8 @@
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
+        <div class="col-md-12">
+            <div class="card card-primary">
                 <div class="card-header">{{ __('Action') }}</div>
 
                 <div class="card-body">
@@ -28,45 +13,299 @@
                             method="POST"
                     >
                     @csrf
-                    <input type="file" name="import_file" />
-                    <button type="submit">Import</button>
+                    <div class="row">
+                        <div class="col-5">
+                            <input type="file" class="form-control" name="import_file" />
+                        </div>
+                        <div class="col-2">
+                            <button class="btn btn-primary" type="submit">Import</button>
+                        </div>
+                    </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="row" style="border-top:20px;">
-        <div class="col-md-12">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Datetime</th>
-                        <th>Site</th>
-                        <th>Initial Account Point</th>
-                        <th>Current Agent Wallet</th>
-                        <th>Current Agent Commission</th>
-                        <th>Total Loading</th>
-                        <th>Total Load Withdrawal</th>
-                        <th>Total Commission Cashout</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($arenaOverview as $index => $value)
-                        <tr>
-                            <td>{{ $value->date }}</td>
-                            <td>{{ $value->site }}</td>
-                            <td>{{ number_format($value->initial_account_point,2) }}</td>
-                            <td>{{ number_format($value->current_agent_wallet,2) }}</td>
-                            <td>{{ number_format($value->current_agent_commission,2) }}</td>
-                            <td>{{ number_format($value->total_loading,2) }}</td>
-                            <td>{{ number_format($value->total_load_withdrawal,2) }}</td>
-                            <td>{{ number_format($value->total_commission_cashout,2) }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    <div class="card card-primary">
+        <div class="card-header">Arena Overview</div>
+
+        <div class="card-body">
+            <div class="row" style="border-top:20px;">
+                <div class="col-md-12">
+                    <table class="table table-striped arena-overview">
+                        <thead>
+                            <tr>
+                                <th>Event Date</th>
+                                <th>Site</th>
+                                <th>Initial Account Point</th>
+                                <th>Current Agent Wallet</th>
+                                <th>Current Agent Commission</th>
+                                <th>Total Loading</th>
+                                <th>Total Load Withdrawal</th>
+                                <th>Total Commission Cashout</th>
+                                <th>Must Total Players Point</th>
+                                <th>Actual Players Point</th>
+                                <th>Total Bets</th>
+                                <th>Total Rake</th>
+                                <th>Rake Without Agent Commission</th>
+                                <th>Player Point Difference</th>
+                                <th>Initial Agent Commission</th>
+                                <th>Total Agent Commission</th>
+                                <th>Total Processed Commission</th>
+                                <th>Total Unprocessed Commission</th>
+                                <th>Commission Difference</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($arenaOverview as $index => $value)
+                                <tr>
+                                    <td>{{ $value->date }}</td>
+                                    <td>{{ $value->site }}</td>
+                                    <td>{{ number_format($value->initial_account_point,2) }}</td>
+                                    <td>{{ number_format($value->current_agent_wallet,2) }}</td>
+                                    <td>{{ number_format($value->current_agent_commission,2) }}</td>
+                                    <td>{{ number_format($value->total_loading,2) }}</td>
+                                    <td>{{ number_format($value->total_load_withdrawal,2) }}</td>
+                                    <td>{{ number_format($value->total_commission_cashout,2) }}</td>
+                                    <td>{{ number_format($value->must_total_players_point,2) }}</td>
+                                    <td>{{ number_format($value->actual_players_point,2) }}</td>
+                                    <td>{{ number_format($value->total_bets,2) }}</td>
+                                    <td>{{ number_format($value->total_rake,2) }}</td>
+                                    <td>{{ number_format($value->rake_without_agent_commission,2) }}</td>
+                                    <td>{{ number_format($value->player_point_difference,2) }}</td>
+                                    <td>{{ number_format($value->initial_agent_commission,2) }}</td>
+                                    <td>{{ number_format($value->total_agent_commission,2) }}</td>
+                                    <td>{{ number_format($value->total_processed_commission,2) }}</td>
+                                    <td>{{ number_format($value->total_unprocessed_commission,2) }}</td>
+                                    <td>{{ number_format($value->commission_difference,2) }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card card-info">
+        <div class="card-header">Arena Overview Total</div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Initial Account Point</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Current Agent Wallet</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Current Agent Commission</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Total Loading</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Total Load Withdrawal</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Total Commission Cashout</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- <div classc="clearfix"></div> -->
+
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Must Total Players Pointt</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Actual Players Point</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Total Bets</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Total Rake</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Total Loading</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Rake Without Agent Commission</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="clearfix"></div> -->
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Player Point Difference</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Initial Agent Commission</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Total Processed Commission</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Total Unprocessed Commission</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-3 overview">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                        <h5>452,450,625.0000</h3>
+
+                        <p>Commission Difference</p>
+                        </div>
+                        <div class="icon">
+                        <i class="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+
+<link rel="stylesheet" href="css/style.css">
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,83 +7,166 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Lucky 8 WPC') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+      <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="/home" class="nav-link">Home</a>
+      </li>
+    </ul>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      
 
-                    </ul>
+      <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fa fa-cog"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+          <div class="dropdown-divider"></div>
+          <a href="/logout" class="dropdown-item">
+            <span class="text-sm">Log out</span>
+          </a>
+          <div class="dropdown-divider"></div>
+        </div>
+      </li>
+      
+    </ul>
+  </nav>
+  <!-- /.navbar -->
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
+      <h3 class="brand-text font-weight-light">WPC2021</h3>
+    </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/default-150x150.png" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Admin</a>
+        </div>
+      </div>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+         
+          <li class="nav-item">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Home
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
     </div>
 </body>
 
 
 
-<script>
+<!-- Scripts -->
+<!-- jQuery -->
+<script src="{{ asset('js/jquery.min.js') }}" ></script>
+<script src="{{ asset('js/bootstrap.min.js') }}" ></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<script type="text/javascript">
     $("document").ready(function(){
-        alert('tes');
+        // alert('tes');
     });
 </script>
 
