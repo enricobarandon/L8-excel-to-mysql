c893ff0e24a2aaca1e771b8e52f0f4bd14cb9977
samSAM <samuelbandola@gmail.com>
2022-04-01 14:12:36 +0800

    request

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
       <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
-      <h3 class="brand-text font-weight-light">LUCKY 8</h3>
+      <h3 class="brand-text font-weight-light">WPC2021</h3>
     </a>
 
     <!-- Sidebar -->
